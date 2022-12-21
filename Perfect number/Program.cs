e1c32d679161c110ddbe5a319d9a0e67287ba938
zai31 <100871938+zai31@users.noreply.github.com>
2022-12-21 18:05:51 +0300

    Add files via upload

---
+++ Perfect number/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perfect_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            Console.WriteLine("Please enter two integers");
+            num1 = int.Parse(Console.ReadLine());
+            num2 = int.Parse(Console.ReadLine());
+            for (int i = num1; i <= num2; i++)
+            { 
+                int sum = 0;
+                for (int j = 1; j < i; j++)
+                {
+                   
+                    if (i % j == 0)
+                    {
+                        sum += j;
+                    }
+                }
+                    if (sum==i)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+       
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ Prime num/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime_num
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1,num2;
+            
+            Console.WriteLine("Inter two integers");
+            num1 = int.Parse(Console.ReadLine());
+            num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Prime numbers are:");
+            for (int i = num1; i <= num2; i++)
+            {
+                bool prime = true;
+                int m = i / 2;
+                if (i <= 1)
+                {
+                    prime = false;
+                }
+                for (int j = 2; j <= m; ++j)
+                {
+
+                    if (i % j == 0)
+                    {
+                        prime = false;
+                        break;
+                    }
+
+                }
+
+
+                if (prime == true)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+                Console.ReadKey();
+            
+        }
+    }
+}
